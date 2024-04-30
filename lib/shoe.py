4e7f1fec0b629436c1538f635495d44504ebe6b1
Julie Poulain <juliempoulain@gmail.com>
2024-04-30 14:10:49 -0700

    add properties and functions

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    condition = ""
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size 
+    
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        self._size = size
